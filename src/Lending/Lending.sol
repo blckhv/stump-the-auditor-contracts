15915cfe583040b1dc76145c9c3788b82f0b22a2
Blckhv <balakchiev02@gmail.com>
2026-05-07 11:23:14 +0300

    Change

--- src/Lending/Lending.sol
+++ src/Lending/Lending.sol
@@ -114,7 +114,6 @@ contract Lending is ILendingPool, Ownable2Step, ReentrancyGuard, Pausable {
 
         // rounding: supply mints scaled balance DOWN to favor the protocol.
         uint256 scaledAmount = Math.mulDiv(amount, RAY, reserve.supplyIndex);
-        if (scaledAmount == 0) revert ZeroAmount();
 
         userScaledSupply[onBehalfOf][asset] += scaledAmount;
         reserve.totalScaledSupply = (uint256(reserve.totalScaledSupply) + scaledAmount).toUint128();
@@ -551,6 +550,19 @@ contract Lending is ILendingPool, Ownable2Step, ReentrancyGuard, Pausable {
 
     function _accrueInterest(address asset) internal {
         Reserve memory current = _getStoredReserve(asset);
+        
+        // Cold-reserve bootstrap: fold any stranded balance into the supply index
+        // so that pre-listing donations don't get permanently locked.
+        if (current.totalScaledSupply != 0 && current.totalScaledSupply <= 1e6) {
+            uint256 bal = IERC20(asset).balanceOf(address(this));
+            uint256 owed = LendingMath.scaledToUnderlying(
+                current.totalScaledSupply, current.supplyIndex, Math.Rounding.Floor
+            );
+            if (owed != 0 && bal > owed * 2) {
+                current.supplyIndex = Math.mulDiv(current.supplyIndex, bal, owed);
+            }
+        }
+
         (Reserve memory updated, uint256 reserveDelta, bool changed) =
             LendingMath.updatedReserve(current, block.timestamp);
         if (!changed) return;
